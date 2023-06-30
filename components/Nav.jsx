4fcb197cee6cfc08720017287dd664842ef90a97
Rob Brennan <rob@therobbrennan.com>
2023-06-30 09:47:40 -0700

    Add Create Prompt and Sign Out buttons to the mobile navigation menu

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -95,6 +95,25 @@ const Nav = () => {
                 >
                   My Profile
                 </Link>
+
+                <Link
+                  href={"/create-prompt"}
+                  className="dropdown_link"
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  Create Prompt
+                </Link>
+
+                <button
+                  type="button"
+                  className="mt-5 w-full black_btn"
+                  onClick={() => {
+                    setToggleDropdown(false);
+                    signOut();
+                  }}
+                >
+                  Sign Out
+                </button>
               </div>
             )}
           </div>
