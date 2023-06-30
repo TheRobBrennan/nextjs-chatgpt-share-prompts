977c81fecb408943cac0d9e684e863e7d5dfcf82
Rob Brennan <rob@therobbrennan.com>
2023-06-30 09:31:40 -0700

    Initial implementation for unauthenticated users

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -5,7 +5,18 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const isUserLoggedIn = false;
+  const [providers, setProviders] = useState(null);
+
+  // On our initial render, let's get a list of all possible authentication providers (Google, GitHub, Facebook, etc.) we support
+  useEffect(() => {
+    const setProviders = async () => {
+      const response = await getProviders();
+      setProviders(response);
+    };
+
+    setProviders();
+  }, []);
 
   return (
     <nav className="flex-between w-full mb-16 pt-3">
@@ -43,7 +54,19 @@ const Nav = () => {
             </Link>
           </div>
         ) : (
-          <></>
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => {
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className="black_btn"
+                >
+                  Sign In
+                </button>;
+              })}
+          </>
         )}
       </div>
     </nav>
