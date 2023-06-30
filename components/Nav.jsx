02cab41ebf85cc5728b81b52d38ca4840203b70d
Rob Brennan <rob@therobbrennan.com>
2023-06-30 09:43:24 -0700

    Add initial mobile navigation

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -5,8 +5,9 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  const isUserLoggedIn = false;
+  const isUserLoggedIn = true;
   const [providers, setProviders] = useState(null);
+  const [toggleDropdown, setToggleDropdown] = useState(false);
 
   // On our initial render, let's get a list of all possible authentication providers (Google, GitHub, Facebook, etc.) we support
   useEffect(() => {
@@ -52,6 +53,50 @@ const Nav = () => {
                 alt="Profile"
               />
             </Link>
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => {
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className="black_btn"
+                >
+                  Sign In
+                </button>;
+              })}
+          </>
+        )}
+      </div>
+
+      {/* Mobile navigation */}
+      <div className="sm:hidden flex relative">
+        {isUserLoggedIn ? (
+          <div className="flex">
+            <Image
+              src={"/assets/images/logo.svg"}
+              width={37}
+              height={37}
+              className="rounded-full"
+              alt="Profile"
+              onClick={() => {
+                setToggleDropdown((prev) => !prev);
+              }}
+            />
+
+            {toggleDropdown && (
+              <div className="dropdown">
+                <Link
+                  href={"/profile"}
+                  className="dropdown_link"
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  My Profile
+                </Link>
+              </div>
+            )}
           </div>
         ) : (
           <>
